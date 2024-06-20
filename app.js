6f2a8444c352dd423697785ab2d4c39d041ea4f5
jpfiscal <jfiscalini@gmail.com>
2024-06-19 22:53:28 -0600

    part 3 of exercise

--- app.js
+++ app.js
@@ -8,33 +8,27 @@ const newDeck = axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck
         deckID = res.data.deck_id;
     })
     .catch(err => {
-        console.log(err)
+        console.log(err);
     });
 
 function drawCard(dID) {
-    cardList= []
     axios.get(`${baseURL}/${dID}/draw/?count=1`)
         .then(res=>{
-            cardList.push(`${res.data.cards[0].value} of ${res.data.cards[0].suit}`);
-            return axios.get(`${baseURL}/${dID}/draw/?count=1`);
-            // console.log(`${res.data.cards[0].value} of ${res.data.cards[0].suit}`);
-        }).then(res=>{
-            cardList.push(`${res.data.cards[0].value} of ${res.data.cards[0].suit}`);
-            showCards(cardList);
+            console.log(res);
+            let drawnCard = document.createElement("img");
+            drawnCard.src = res.data.cards[0].image;
+            cardDiv.appendChild(drawnCard);
+            cardsLeft = res.data.remaining;
+            console.log(`Cards Left in Deck: ${cardsLeft}`)
+            if (cardsLeft == 0){
+                btn.style.display = "none";
+            }
         }).catch(err => {
             console.log(err);
         })
 }
 
-function showCards(cardList){
-    let i = 0;
-    while (i < cardList.length){
-        console.log(cardList[i])
-        i++;
-    }
-}
-
 btn.addEventListener('click', function(e){
-    e.preventDefault();
+    // e.preventDefault();
     drawCard(deckID);
 })
